36093054f12be396e831c94108a229d4d98ae1d8
jfreissmann <j.freissmann@web.de>
2021-02-02 11:58:49 +0100

    Add information to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup
 setup(
     name='SWSHplotting',
     version='0.0.1',
+    author='Jonas Freißmann',
+    author_email='jonas.freissmann@hs-flensburg.de',
     description='Plotting package for SWSH project.',
+    url='https://github.com/jfreissmann/SWSHplotting',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+        ],
     py_modules=['SWSHplotting'],
     package_dir={'': 'src'}
 )
