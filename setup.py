6a08f88ce836e9ef446ef744a94c330129fb2932
jfreissmann <j.freissmann@web.de>
2021-02-02 10:36:07 +0100

    Add rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='SWSHplotting',
+    version='0.0.1',
+    description='Plotting package for SWSH project.',
+    py_modules=['SWSHplotting'],
+    package_dir={'': 'src'}
+)
