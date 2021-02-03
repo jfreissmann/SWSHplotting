5eaa6f3fdffe21380f94f019e91bdb5b5144b905
jfreissmann <j.freissmann@web.de>
2021-02-03 09:23:14 +0100

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open('README.rst', 'r') as f:
+    long_discription = f.read()
+
 setup(
     name='SWSHplotting',
     version='0.0.1',
     author='Jonas Freißmann',
     author_email='jonas.freissmann@hs-flensburg.de',
     description='Plotting package for SWSH project.',
+    long_discription=long_discription,
+    long_discription_content_type='text/x-rst',
     url='https://github.com/jfreissmann/SWSHplotting',
     license='MIT',
     classifiers=[
